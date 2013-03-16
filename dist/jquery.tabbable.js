e87e347b53704c4c21728552717e3872925fbfbc
Damian Nicholson <damian.nicholson21@gmail.com>
2013-03-16 22:19:12 +0000

    Added a jQuery plugin manifest

--- dist/jquery.tabbable.js
+++ dist/jquery.tabbable.js
@@ -4,6 +4,14 @@
 
 (function($) {
 
+  /**
+   * Contains a list of focussable elements
+   *
+   * @property FOCUSSABLE_ELEMS
+   * @type Array
+   * @static
+   * @final
+   */
   var FOCUSSABLE_ELEMS = [
     'input',
     'select',
@@ -11,19 +19,42 @@
     'button'
   ];
 
-  // Custom selector.
+  /**
+   * Adds a tabbable pseudo selector to jQuery
+   *
+   * @example
+   *   $('form :input').filter(':tabbable');
+   */
   $.expr[':'].tabbable = function(elem) {
     return isTabbable(elem) && isFocussable(elem) && isVisible(elem);
   };
 
+  /**
+   * @method isTabbable
+   * @param {Node}
+   * @return {Boolean}
+   * @private
+   */
   function isTabbable(elem) {
     return elem.tabIndex >= 0;
   }
 
+  /**
+   * @method isVisible
+   * @param {Node}
+   * @return {Boolean}
+   * @private
+   */
   function isVisible(elem) {
     return $(elem).is(':visible');
   }
 
+  /**
+   * @method isFocussable
+   * @param {Node}
+   * @return {Boolean}
+   * @private
+   */
   function isFocussable(elem) {
     var node = elem.nodeName,
         regex = new RegExp(FOCUSSABLE_ELEMS.join('|'), 'gi');
